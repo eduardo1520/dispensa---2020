60c9c9ed8fd7afca99de2e078986f9c3005cd8f6
Eduardo Oliveira <eduardomoliveira@apoioprodesp.sp.gov.br>
2020-10-28 19:49:01 -0300

    Trabalhando no javascript

--- public/js/productsRequest.js
+++ public/js/productsRequest.js
@@ -53,62 +53,92 @@ function detectar_mobile() {
     var check = false; //wrapper no check
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
     if(check == true) {
-        $("#page-top").addClass();
-        $("#accordionSidebar").removeClass('accordion').addClass('toggled');
-        if($(window).width() <= 320) {
-            $("#filho").removeClass().addClass('col-12 border mt-3');
-            $("#tabela").find('div.row').children('div').each(function () {
-                $(this).css('padding-left','5px');
-                $(this).css('padding-right','5px');
+        const body = document.querySelectorAll("body#page-top");
+        body.forEach(function(value){
+            value.classList.remove('sidebar-toggled');
+        });
+        const accordion = document.querySelectorAll("ul#accordionSidebar");
+        accordion.forEach(function(value){
+            value.classList.remove('accordion');
+            value.classList.add('toggled');
+        });
+        if(window.screen.width <= 320) {
+            const tabela = document.querySelectorAll('#tabela > #filho > div.row > div');
+            tabela.forEach(function(value){
+               value.style['padding-left'] = '5px';
+               value.style['padding-right'] = '5px';
             });
-        }
 
-        if($(window).width() <= 568) {
-            $('.user').hide();
-            $('.detalhe').hide();
-            $('.produto').removeAttr('onclick')
-                          .attr('data-toggle',"modal")
-                          .attr('data-target',".modalRequestProduct")
-                          .attr('onclick',"abreModalRequestProduct()");
-            $(".gj-datepicker").hide();
-
-            $('.data').attr('onclick','habilitaData();');
             let hoje = new Date();
 
-            $(".data").attr('data-toggle',"modal").attr('data-target',".modalData");
-            $(".data").append(`<span class="selecionado">${hoje.getDate()}/${hoje.getMonth() + 1}/${hoje.getFullYear()}</span>`);
+            const data = document.querySelectorAll('div.data div.gj-datepicker');
+            console.log(data[0].nextElementSibling);
+
+            // data.forEach(function(div){
+            //     console.log(div);
+                // div.remove('span');
+                // div.insertAdjacentHTML('beforeend',`<span class="selecionado">${hoje.getDate()}/${hoje.getFullYear()}</span>`);
+            // });
 
         }
-
-        if($(window).width() > 568) {
-            $('.user').show();
-            $('.detalhe').remove();
+        if(window.screen.width <= 568) {
+            const user = document.querySelectorAll('div.user');
+            user.forEach(function(div){
+                div.classList.add('d-none');
+            });
+            const detalhe = document.querySelectorAll('div.detalhe');
+            detalhe.forEach(function(div){
+                div.classList.add('d-none');
+            });
+            const produto = document.querySelectorAll('div.produto');
+            produto.forEach(function(div){
+                div.setAttribute('onclick','');
+                div.setAttribute('data-toggle','modal');
+                div.setAttribute('data-target','.modalRequestProduct');
+                div.setAttribute('onclick',"abreModalRequestProduct()");
+            });
+            const datepicker = document.querySelectorAll('div.pedido > div.data > div.gj-datepicker');
+            datepicker.forEach(function(div){
+                div.classList.add('d-none');
+            });
+            let hoje = new Date();
+            const data = document.querySelectorAll('div.data');
+            data.forEach(function(div){
+                div.setAttribute('onclick','habilitaData()');
+                div.setAttribute('data-toggle',"modal");
+                div.setAttribute('data-target',".modalData");
+                div.insertAdjacentHTML('beforeend',`<span class="selecionado">${hoje.getDate()}/${hoje.getMonth() + 1}/${hoje.getFullYear()}</span>`);
+            });
+        }
+        if(window.screen.width > 568) {
+           document.querySelector('.user').classList.add('d-block');
+           document.querySelector('.detalhe').remove();
+            // $('.detalhe').remove();
         }
     }
 }
-// TODO
+
 function getProductImage(id, name) {
-    $.ajax({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        url: `product/productImageAjax`,
-        type: 'post',
-        dateType: 'json',
-        data:{
-            id: id,
-            name: name
-        },
-        success: function(res) {
-            $(".imagem").empty().append(`<img src="${res[0].image}" alt="${res[0].description}" data-id="${res[0].id}" width="100px;" height="75px;">`);
-            $(".imagem-produto").removeClass('d-none');
-            $(".imagem-nome").text(res[0].name);
-            $(".imagem-descricao").text(res[0].description);
-        },
-        error: function (error) {
-            console.log(error);
-        }
-    });
+    promise(`product/productImageAjax`,'post', {id:id, name:name})
+        .then(response => {
+            return response.json();
+        })
+        .then(produto => {
+            const imagem = document.querySelectorAll('.pedido > div.imagem');
+            imagem.forEach(function(value){
+                value.innerHTML = "";
+                value.insertAdjacentHTML('beforeend', `<img src="${produto[0].image}" alt="${produto[0].description}" data-id="${produto[0].id}" width="100px;" height="75px;">`);
+            });
+            document.querySelector('.imagem-produto').classList.remove('d-none');
+            const imagem_produto = document.querySelectorAll('.imagem-produto > .cabecalho > .imagem');
+            imagem_produto.forEach(function(value){
+                value.classList.remove('d-none');
+                value.innerHTML = "";
+                value.insertAdjacentHTML('beforeend', `<img src="${produto[0].image}" alt="${produto[0].description}" data-id="${produto[0].id}" width="100px;" height="75px;">`);
+            });
+           document.querySelector('.imagem-nome').innerHTML = produto[0].name;
+           document.querySelector('.imagem-descricao').innerHTML = produto[0].description;
+        });
 }
 
 function promise(url, method, params) {
@@ -158,7 +188,7 @@ function comboProduto() {
         }).then(combo => {
             let element = document.querySelector(".combo-prod");
             combo.forEach(function(value){
-                element.insertAdjacentHTML('beforeend', `<option value="${value['id']}">${value['name']})</option>`);
+                element.insertAdjacentHTML('beforeend', `<option value="${value['id']}">${value['name']}</option>`);
             });
         }).catch(error => {
             console.log('Deu erro,', error);
@@ -183,10 +213,8 @@ function getCategory(classe, pai,produto,campo) {
 function setData() {
     let d = new Date();
     let hoje = `${d.getDate()}/${d.getMonth() +1}/${d.getFullYear()}`
-
     const data = document.querySelector('.date');
     data.setAttribute('value', (document.querySelector('#datetimepicker1').value != '') ? document.querySelector('#datetimepicker1').value : hoje);
-
     const selecionado = document.querySelector('.selecionado');
     selecionado.insertAdjacentHTML('beforeend', (document.querySelector('#datetimepicker1').value != '') ? document.querySelector('#datetimepicker1').value : hoje);
 }
@@ -242,14 +270,11 @@ function habilitaData() {
     elemento.forEach(function(value){
         value.classList.add("d-none");
     });
-
-    const selecionado = document.querySelector(('.selecionado'));
+    const selecionado = document.querySelector('.selecionado');
     selecionado.innerHTML = "";
     selecionado.setAttribute('value',document.querySelector('#datetimepicker1'));
-
     const datepicker = document.querySelector('#admin > .gj-datepicker');
     datepicker.setAttribute('style','');
-
     const admin = document.querySelectorAll('#admin > div.gj-datepicker > span');
     admin.forEach(function(value){
         value.classList.remove('d-none');
